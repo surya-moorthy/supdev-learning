1722611a7c7560a24200b9b5d3825cf19b602bcc
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-07-18 10:28:45 +0530

    custom macro in derive macros

--- superdev-1/src/main.rs
+++ superdev-1/src/main.rs
@@ -1,19 +1,57 @@
-#[derive(Debug)] // debug trait as a custom derive macro 
-struct User {
-    name : String,
-    age : u32   
+use std::{fmt::Error, vec};
+
+trait  Serialize {
+    fn serialize(&self) -> Vec<u8>;
+}
+ 
+trait Deserialize {
+    fn deserialize(v : Vec<u8>) -> Result<Swap,Error>;
+}
+
+#[derive(Debug)]
+struct  Swap {
+    qty1 : u32,
+    qty2 : u32
+}
+
+impl Serialize for Swap {
+    fn serialize(&self) -> Vec<u8> {
+        let mut v = vec![];
+        v.extend_from_slice(&self.qty1.to_be_bytes());  // 1 -> [0,0,0,1]   for to_le_bytes 1 -> [1,0,0,0]
+        v.extend_from_slice(&self.qty2.to_be_bytes());
+        return v;
+    }
+}
+
+impl Deserialize for Swap {
+
+
+  fn deserialize(v : Vec<u8>) -> Result<Swap,Error> {
+      if v.len() < 8 {
+          return Err(Error);
+      };    
+      
+      let qty1  = u32::from_be_bytes([v[0],v[1],v[2],v[3]]);
+      let qty2  = u32::from_be_bytes([v[4],v[5],v[6],v[7]]);
+
+      return Ok(
+        Swap {
+            qty1,
+            qty2
+        }
+      );
+  } 
 }
 
 fn main() {
-    let u = User {
-        name : "Surya".to_string(),
-        age : 18 
-    };  
+    let s = Swap {
+        qty1 : 4,
+        qty2 : 5
+    };
+    let v = s.serialize();
+    println!("{:?}",v);
 
-    println!("{:?}",u); // declarative macro
-}
+    let dev = Swap::deserialize(v);
+    println!("{:?}",dev);
 
-#[post("/api/users")]  // attribute like procedural macro
-fn get_users(){
-    sqlx::query!("INSERT INTO USERS VALUES ()");  //function like procedural macro 
 }
